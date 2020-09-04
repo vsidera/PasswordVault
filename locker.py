9bc1c9e79b9ac3babcff2533d00215e81628c959
vsidera <siderravictor@gmail.com>
2020-09-04 13:07:41 +0300

    test instantiation of credentials object

--- locker.py
+++ locker.py
@@ -4,7 +4,27 @@ class User :
     """
     user_list = [] # Empty user list
 
-    def __init__(self,username,password):
+    def __init__(self,login,loginkey):
+
+        """
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            login : New user login.
+            loginkey : New user loginkey.
+        """
+
+        self.login = login
+        self.loginkey = loginkey
+
+#Credentials class
+class Credentials :
+    """
+    Class that creates new instances of credentials
+    """
+    credentials_list = [] # Empty credentials list
+
+    def __init__(self,account,username,password):
 
         """
         __init__ method that helps us define properties for our objects.
@@ -13,6 +33,6 @@ class User :
             username : New user username.
             password : New user password.
         """
-
+        self.account = account
         self.username = username
-        self.password = password
+        self.password = password        
--- locker_test.py
+++ locker_test.py
@@ -1,5 +1,6 @@
 import unittest # Importing the unittest module
 from locker import User #Importing the User class
+from locker import Credentials #Importing the Credentials class
 
 class TestUser(unittest.TestCase):
     """
@@ -14,14 +15,19 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("vik","P@pi") # create user object
+        self.new_credentials = Credentials("facebook","sidera","Fr1day") #create credentials object
 
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_user.username,"vik")
-        self.assertEqual(self.new_user.password,"P@pi")
-       
+        self.assertEqual(self.new_user.login,"vik")
+        self.assertEqual(self.new_user.loginkey,"P@pi")
+
+        self.assertEqual(self.new_credentials.account,"facebook")
+        self.assertEqual(self.new_credentials.username,"sidera")
+        self.assertEqual(self.new_credentials.password,"Fr1day") 
+        
 if __name__ == '__main__':
     unittest.main()    
