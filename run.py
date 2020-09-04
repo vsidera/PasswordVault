b499bc1c18e2669b6bbf00f0f67957b933e4814a
vsidera <siderravictor@gmail.com>
2020-09-04 23:08:11 +0300

    defines new credentials

--- run.py
+++ run.py
@@ -10,6 +10,13 @@ def create_user(login,loginkey):
         new_user = User(login,loginkey)
         return new_user
 
+def create_credentials(account,username,password):
+        """
+        Function to create new credentials
+        """
+        new_credentials = Credentials(account,username,password)
+        return new_credentials        
+
 def save_user(user):
         '''
         Function to save credentials
@@ -54,18 +61,28 @@ def main() :
         confirm = input()
         if confirm == loginkey:
             print("\n You are successfully logged in!")
+        # else:
+        #     print("Sorry,wrong password")  
+
+         
+            while True:
+                        print("-"*10)
+                        print("What would you like to do? \n Pick a short code : \n \t\t aa - Add an account & create your password \n \t\t ga - Generate a password for your new account \n \t\t da - display saved accounts \n \t\t sa - Search for an account \n \t\t D - delete an account \n \t\t ex - Exit the locker room ")
+                        short_code = input().lower()
         
-        # print("Great!Please proceed to login")
-        # print("Enter username")
-
-        # print(f"Great !What would you like to do { login }?")
-        # print('\n')
-        
-        # while True:
-        #             print("Choose a short code : ca - create a new credential, dc - display credentials, fc -find credential, ex -exit credential list ")
-
-        #             short_code = input().lower()
-
+                        if short_code == 'aa':
+                            print("-"*10)
+                            print("Enter account name")
+                            print("Account name ....(eg: facebook)")
+                            account = input()
+                            print("Create username")
+                            username = input()
+                            print("Create password")
+                            password = input()
+                            save_credentials(create_credentials(account, u_name, passwd))
+                            print ('\n')
+                            print(f"New account: {account} added \n Username: {username} --- Password:{password}")
+                            print ('\n')
 
 if __name__ == '__main__':
 
